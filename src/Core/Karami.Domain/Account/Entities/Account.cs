e2571f8258b1d2f6fb0e402e7bae7372984719b5
Hasan Karami Moheb <hasankarami2020313@gmail.com>
2024-01-18 16:14:03 +0330

    updated packages versions

--- src/Core/Karami.Domain/Account/Entities/Account.cs
+++ src/Core/Karami.Domain/Account/Entities/Account.cs
@@ -1,13 +1,19 @@
 #pragma warning disable CS0649
 
 using Karami.Core.Domain.Contracts.Abstracts;
+using Karami.Core.Domain.Contracts.Interfaces;
 using Karami.Core.Domain.Enumerations;
+using Karami.Core.Domain.ValueObjects;
+using Karami.Domain.Service.Events;
+using Karami.Domain.Service.ValueObjects;
 
 namespace Karami.Domain.Service.Entities;
 
 public class Account : Entity<string>
 {
     //Value Objects
+    
+    public Balance Balance { get; private set; }
 
     /*---------------------------------------------------------------*/
     
@@ -18,6 +24,19 @@ public class Account : Entity<string>
     //EF Core
     private Account() {}
 
+    public Account(IGlobalUniqueIdGenerator globalUniqueIdGenerator, IDateTime dateTime, long balance)
+    {
+        var uniqueId = globalUniqueIdGenerator.GetRandom();
+        var nowDateTime = DateTime.Now;
+        var nowPersianDateTime = dateTime.ToPersianShortDate(nowDateTime);
+
+        Id = uniqueId;
+        CreatedBy = uniqueId;
+        Balance = new Balance(balance);
+        CreatedAt = new CreatedAt(nowDateTime, nowPersianDateTime);
+        UpdatedAt = new UpdatedAt(nowDateTime, nowPersianDateTime);
+    }
+
     /*---------------------------------------------------------------*/
     
     //Behaviors
@@ -25,31 +44,42 @@ public class Account : Entity<string>
     /// <summary>
     /// 
     /// </summary>
-    public void Active()
+    public void Active(IDateTime dateTime)
     {
-        IsActive = IsActive.Active;
+        var nowDateTime = DateTime.Now;
+        var nowPersianDateTime = dateTime.ToPersianShortDate(nowDateTime);
         
-        /*AddEvent(
+        IsActive  = IsActive.Active;
+        UpdatedAt = new UpdatedAt(nowDateTime, nowPersianDateTime);
+        
+        AddEvent(
             new AccountActived {
-                Id = Id ,
+                Id = Id,
                 IsActive = IsActive == IsActive.Active,
-                UpdatedAt_EnglishDate = 
+                UpdatedAt_EnglishDate = nowDateTime,
+                UpdatedAt_PersianDate = nowPersianDateTime
             }
-        );*/
+        );
     }
     
     /// <summary>
     /// 
     /// </summary>
-    public void InActive()
+    public void InActive(IDateTime dateTime)
     {
-        IsActive = IsActive.InActive;
+        var nowDateTime = DateTime.Now;
+        var nowPersianDateTime = dateTime.ToPersianShortDate(nowDateTime);
         
-        /*AddEvent(
-            new TemplateInActived {
-                Id       = Id ,
-                IsActive = IsActive == IsActive.Active
+        IsActive  = IsActive.InActive;
+        UpdatedAt = new UpdatedAt(nowDateTime, nowPersianDateTime);
+        
+        AddEvent(
+            new AccountInActived {
+                Id = Id,
+                IsActive = IsActive == IsActive.Active,
+                UpdatedAt_EnglishDate = nowDateTime,
+                UpdatedAt_PersianDate = nowPersianDateTime
             }
-        );*/
+        );
     }
 }
