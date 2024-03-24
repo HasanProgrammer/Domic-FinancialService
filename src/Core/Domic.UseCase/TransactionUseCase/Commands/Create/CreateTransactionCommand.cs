c25b45bec2cfb2ddd1bd5325381ddcd9e048c2fe
Hasan Karami Moheb <hasankarami2020313@gmail.com>
2024-03-24 14:01:23 +0330

    update packages

---
+++ src/Core/Domic.UseCase/TransactionUseCase/Commands/Create/CreateTransactionCommand.cs
@@ -0,0 +1,13 @@
+﻿using Domic.Core.UseCase.Contracts.Interfaces;
+using Domic.Domain.Transaction.Enumerations;
+
+namespace Domic.UseCase.TransactionUseCase.Commands.Create;
+
+public class CreateTransactionCommand : ICommand<bool>
+{
+    public required string AccountId { get; init; }
+    public required string TransactionId { get; init; }
+    public required long? IncreasedAmount { get; init; }
+    public required long? DecreasedAmount { get; init; }
+    public required TransactionType TransactionType { get; init; }
+}
---
+++ src/Core/Domic.UseCase/TransactionUseCase/Commands/Create/CreateTransactionCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using Domic.Core.Domain.Contracts.Interfaces;
+using Domic.Core.UseCase.Attributes;
+using Domic.Core.UseCase.Contracts.Interfaces;
+using Domic.Domain.Transaction.Contracts.Interfaces;
+using Domic.Domain.Transaction.Entities;
+
+namespace Domic.UseCase.TransactionUseCase.Commands.Create;
+
+public class CreateTransactionCommandHandler(
+    ITransactionCommandRepository transactionCommandRepository, IGlobalUniqueIdGenerator globalUniqueIdGenerator,
+    IDateTime dateTime
+) : ICommandHandler<CreateTransactionCommand, bool>
+{
+    [WithTransaction]
+    public Task<bool> HandleAsync(CreateTransactionCommand command, CancellationToken cancellationToken)
+    {
+        var newTransaction = new Transaction(globalUniqueIdGenerator, dateTime, command.AccountId,
+            command.TransactionId, command.IncreasedAmount, command.DecreasedAmount, command.TransactionType,
+            "", ""
+        );
+        
+        transactionCommandRepository.Add(newTransaction);
+
+        return Task.FromResult(true);
+    }
+}
