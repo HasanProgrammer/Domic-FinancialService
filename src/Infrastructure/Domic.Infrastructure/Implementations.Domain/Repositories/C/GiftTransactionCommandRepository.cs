cde6c54e20b225108ea27b42a133c05bceb35291
Hasan1375-tech <karami.samdad@gmail.com>
2024-09-07 11:04:49 +0330

    update packages

---
+++ src/Infrastructure/Domic.Infrastructure/Implementations.Domain/Repositories/C/GiftTransactionCommandRepository.cs
@@ -0,0 +1,8 @@
+﻿using Domic.Domain.GiftTransaction.Contracts.Interfaces;
+
+namespace Domic.Infrastructure.Implementations.Domain.Repositories.C;
+
+public class GiftTransactionCommandRepository : IGiftTransactionCommandRepository
+{
+    
+}
