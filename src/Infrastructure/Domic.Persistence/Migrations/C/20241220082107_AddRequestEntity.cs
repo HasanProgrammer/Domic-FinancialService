ecdd41b22f1de49923ea699d929a06961d8b96d2
Hasan Karami <hasankarami2020313@gmail.com>
2024-12-20 11:57:18 +0330

    update

---
+++ src/Infrastructure/Domic.Persistence/Migrations/C/20241220082107_AddRequestEntity.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Domic.Persistence.Migrations.C
+{
+    public partial class AddRequestEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Requests",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    AccountId = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    Amount = table.Column<long>(type: "bigint", nullable: true),
+                    Status = table.Column<byte>(type: "tinyint", nullable: false),
+                    RejectReason = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    BankTransferReceiptImage = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UpdatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedRole = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UpdatedRole = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsDeleted = table.Column<byte>(type: "tinyint", nullable: false),
+                    Version = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsActive = table.Column<byte>(type: "tinyint", nullable: false),
+                    CreatedAt_EnglishDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedAt_PersianDate = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UpdatedAt_EnglishDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    UpdatedAt_PersianDate = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Requests", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Requests_Accounts_AccountId",
+                        column: x => x.AccountId,
+                        principalTable: "Accounts",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Requests_AccountId",
+                table: "Requests",
+                column: "AccountId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Requests_Id_IsDeleted",
+                table: "Requests",
+                columns: new[] { "Id", "IsDeleted" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Requests");
+        }
+    }
+}
